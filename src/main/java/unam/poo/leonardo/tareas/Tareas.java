341699f3aa39c56dd781da9e89938723efe34852
LeonardoDDS <dan-130@hotmail.com>
2023-02-22 20:34:26 -0600

    Tarea 3

---
+++ src/main/java/unam/poo/leonardo/tareas/Tareas.java
@@ -0,0 +1,13 @@
+package unam.poo.leonardo.tareas;
+public class Tareas {
+
+    public static void main(String[] args) {
+        for (int r = 0; r < 3; r++) {
+            for (int b = 0; b < 1; b++) {
+                System.out.println("____ ____ ____");
+            }
+            System.out.printf("%5s %4s %n", "|", "|");
+        }
+            System.out.printf("____ ____ ____");
+    }
+}
